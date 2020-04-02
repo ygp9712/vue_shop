eaa255f8e2631055e518f4f18f0389f1b3bbd2d8
Yang <1175430716@qq.com>
2020-04-03 01:43:25 +0800

    登录界面初步实现

---
+++ src/components/login.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <!-- 头像区域 -->
+      <div class="avatar-box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <!-- 登录区域 -->
+      <el-form class="login-form"  :model="form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input v-model="form.userName"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input v-model="form.userPassword"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      form: {
+        userName: '',
+        userPassword: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .login-container
+    display: block;
+    background: #2b4b6b;
+    width: 100%;
+    height: 100%;
+    .login-box
+      width: 450px;
+      height: 300px;
+      background-color: white;
+      border-radius: 3px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      .avatar-box
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        height: 130px;
+        width: 130px;
+        border-radius: 50%;
+        padding: 10px;
+        border: 2px solid #eee;
+        overflow: hidden;
+        box-shadow: 0 0 10px #ddd
+        background: white;
+        img
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+          background: #eee;
+    .login-form
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 0 20px;
+      box-sizing: border-box;
+    .btns
+      display: flex;
+      justify-content: flex-end;
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+// 导入全局样式
+import './assets/css/global.styl'
 Vue.config.productionTip = false
 
 new Vue({
